8357b4eb1b17d265ac32042a6e472e2d8441fbce
jaiatearth <jayashree.sakthivel@gmail.com>
2019-02-05 23:15:28 +0530

    Updates

---
+++ precache-manifest.58717ffa3baab126274ceb8d549b4644.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "0fb99bdc7558e9225bec",
+    "url": "/jaiatearth/react-boilerplate/static/css/main.f468e6d9.chunk.css"
+  },
+  {
+    "revision": "0fb99bdc7558e9225bec",
+    "url": "/jaiatearth/react-boilerplate/static/js/main.0fb99bdc.chunk.js"
+  },
+  {
+    "revision": "f4bb04f8a4ae323e35e0",
+    "url": "/jaiatearth/react-boilerplate/static/js/1.f4bb04f8.chunk.js"
+  },
+  {
+    "revision": "7d619bad772cd97359ee",
+    "url": "/jaiatearth/react-boilerplate/static/js/runtime~main.7d619bad.js"
+  },
+  {
+    "revision": "e1514ac7f6e2ad364a90291084c2d287",
+    "url": "/jaiatearth/react-boilerplate/index.html"
+  }
+];
